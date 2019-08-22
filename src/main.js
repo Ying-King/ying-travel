007a2cfe53a05f800ad40706b5b4f11cf339b8e5
Ying-King <lixuying20@gmail.com>
2019-08-22 17:20:08 +0800

    change

--- src/main.js
+++ src/main.js
@@ -5,9 +5,12 @@ import router from './router';
 import './assets/scss/index.scss';
 
 import fastclick from 'fastclick';
+import VueAwesomeSwiper from 'vue-awesome-swiper';
+import 'swiper/dist/css/swiper.css';
 
 Vue.config.productionTip = false;
 fastclick.attach(document.body);
+Vue.use(VueAwesomeSwiper);
 
 /* eslint-disable no-new */
 new Vue({
--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
     <home-header/>
+    <home-swiper/>
     Home
   </div>
 </template>
 <script>
   import HomeHeader from './components/header';
+  import HomeSwiper from './components/swiper';
   export default {
     name: 'Home',
     components: {
-      HomeHeader
+      HomeHeader,
+      HomeSwiper
     }
   };
 
