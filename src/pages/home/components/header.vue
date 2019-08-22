fef194f189ec0c25995d3513a6835452d764454a
Ying-King <lixuying20@gmail.com>
2019-08-22 15:56:32 +0800

    add: HomeHeader

---
+++ src/pages/home/components/header.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <a class="iconfont back-icon" href="#">&#xe624;</a>
+    </div>
+    <div class="header-input">
+      <a href="#">
+        <i class="iconfont">&#xe632;</i>
+        输入城市/景点/游玩主题
+      </a>
+    </div>
+    <div class="header-right">
+      <a href="#">
+        城市
+        <i class="iconfont arrow-icon">&#xe64a;</i>
+      </a>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'HomeHeader'
+  };
+
+</script>
+<style lang="scss" scoped>
+  @import '~assets/scss/mixins';
+
+  .header {
+    display: flex;
+    line-height: $headerHeight;
+    background: $bgColor;
+
+    &-left {
+      float: left;
+      height: 100%;
+      width: .64rem /* 32/50 */;
+      font-size: .4rem /* 20/50 */;
+      font-weight: bold;
+      text-align: center;
+      a {
+        color: $white;
+      }
+    }
+
+    &-input {
+      flex: 1;
+      height: .64rem /* 32/50 */;
+      line-height: .64rem /* 32/50 */;
+      margin: .14rem /* 7/50 */;
+      padding-left: .1rem /* 5/50 */;
+
+      background: $white;
+      border-radius: .1rem /* 5/50 */;
+      a {
+        color: $grey;
+      }
+    }
+
+    &-right {
+      float: right;
+      height: 100%;
+      width: 1.24rem /* 62/50 */;
+      text-align: center;
+
+      a {
+        color: $white;
+        .arrow-icon {
+          margin-left: -.04rem /* 2/50 */;
+          font-size: .24rem /* 12/50 */;
+      }
+      }
+    }
+  }
+</style>
--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,11 +1,16 @@
 <template>
   <div>
+    <home-header/>
     Home
   </div>
 </template>
 <script>
+  import HomeHeader from './components/header';
   export default {
-    name: 'Home'
+    name: 'Home',
+    components: {
+      HomeHeader
+    }
   };
 
 </script>
