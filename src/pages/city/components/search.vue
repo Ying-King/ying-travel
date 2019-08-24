96771eb6f483c796461618a01192f06cd804d887
Ying-King <lixuying20@gmail.com>
2019-08-24 15:13:21 +0800

    add:search-result

--- src/pages/city/components/search.vue
+++ src/pages/city/components/search.vue
@@ -1,11 +1,80 @@
 <template>
-  <div class="search">
-    <input class="search-input" type="text" placeholder="输入城市名或拼音">
+  <div>
+    <div class="search">
+      <input class="search-input"
+        type="text"
+        placeholder="输入城市名或拼音"
+        v-model="keyword"
+      >
+    </div>
+    <div class="search-content border-bottom"
+      ref="result"
+      v-show="keyword"
+    >
+      <ul>
+        <li class="search-item"
+          v-for="item of list"
+          :key="item.id"
+          @click="handleCityClick(item.name)"
+        >
+          {{item.name}}
+        </li>
+        <li class="search-item border-bottom" v-show="hasNoData">
+          没有找到匹配数据
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 <script>
+  import BScroll from '@better-scroll/core';
   export default {
-    name: 'CitySearch'
+    name: 'CitySearch',
+    props: {
+      cities: Object
+    },
+    data() {
+      return {
+        keyword: '',
+        list: [],
+        timer: null
+      };
+    },
+    computed: {
+      hasNoData() {
+        return !this.list.length;
+      }
+    },
+    watch: {
+      keyword() {
+        if (this.timer) {
+          clearTimeout(this.timer);
+        }
+        if (!this.keyword) { // 清空 input
+          this.list = [];
+          return;
+        }
+        this.timer = setTimeout(() => {
+          const result = [];
+          for (let i in this.cities) {
+            this.cities[i].forEach((value) => {
+              if (value.spell.indexOf(this.keyword) > -1) {
+                result.push(value);
+              }
+            });
+          }
+          this.list = result;
+        }, 100);
+      }
+    },
+    methods: {
+      handleCityClick(city) {
+        this.$router.push('/');
+      }
+    },
+    updated() {
+      this.scroll = new BScroll(this.$refs.result);
+    }
   };
 
 </script>
@@ -24,6 +93,24 @@
       padding: 0 .1rem /* 5/50 */;
       border-radius: .06rem /* 3/50 */;
       text-align: center;
+    }
+  }
+
+  .search-content {
+    overflow: hidden;
+    position: absolute;
+    top: 1.58rem /* 79/50 */;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: 1;
+    background: #eee;
+
+    .search-item {
+      padding-left: .2rem /* 10/50 */;
+      line-height: .6rem /* 30/50 */;
+      color: $textColor;
+      background: $white;
     }
   }
 
